5df23c4b2c58fe5b23ed8f3c8eec1f84e665827c
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2022-12-13 17:39:25 +0700

    :mag: SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,30 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
+      <NextSeo
+      title="Chaiyapat Oam"
+      titleTemplate="Chaiyapatoam"
+      defaultTitle="chaiyapatoam"
+      description="A College Student at KMUTT & FullStack Developer Portfolio Website"
+      canonical="https://chaiyapatoam.vercel.app/"
+      openGraph={{
+        url:"https://chaiyapatoam.vercel.app/",
+        title:"Chaiyapat Oam",
+        description:"A College Student at KMUTT & FullStack Developer",
+        images: [
+          {
+            url:"/chaiyapatoam.jpg",
+            width:800,
+            height:420,
+            alt:"ChaiyapatOam"
+          }
+        ]
+      }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
